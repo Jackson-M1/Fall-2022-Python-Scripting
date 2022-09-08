5abced34fa21acdc1d8fb5ec4e608cd4219d3fa0
iimckenney-matc <iimckenney@madisoncollege.edu>
2022-09-08 14:15:28 -0500

    Your First Script lab - ready for grading

---
+++ week2/first-script.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+#Author: Jackson Mckenney
+
+#This script is my first python script and will output one thing I've learned about python within this module
+#and will ask you for your first name and age then output a message to you
+
+print("One thing I've learned about python within this module is how to efficiently format strings with variables")
+print("")
+print("What is your full name?")
+userName = input()
+print("")
+print("How old are you?")
+userAge = input()
+print("")
+print(f"Well hello {userName}! {userAge} is a ripe young age to begin learning python")
